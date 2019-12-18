0cfba6d2ecec37afbc15680c916f12589e51c725
jeerawat-hash <s6202041520016@email.kmutnb.ac.th>
2019-12-18 23:16:43 +0700

    ter

---
+++ application/views/page/login.php
@@ -0,0 +1,80 @@
+ 
+<head>
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SMC (Thailand) Ltd.  </title>
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="Untitled-2.png">
+
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/flag-icon.min.css">
+    <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap-select.less"> -->
+    <link rel="stylesheet" href="assets/scss/style.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+
+</head>
+<body class="bg-info">
+
+
+    <div class="sufee-login d-flex align-content-center flex-wrap">
+        <div class="container">
+            <div class="login-content">
+                <div class="login-logo">
+                    <a href="home.php">
+                        <img class="align-content" src="images/SMC_white398px.png" alt=""  >
+                    </a>
+                </div>
+                <div class="login-form">
+                  <form method ="post"  action="./login_query.php">
+                        <div class="form-group">
+                            <label>ชื่อผู้ใช้/Username :</label>
+                            <input type="text" class="form-control" placeholder="Username"  name="member_code"  >
+                        </div>
+                        <div class="form-group">
+                            <label>รหัสผ่าน/Password :</label>
+                            <input type="password" class="form-control" placeholder="Password"  name="member_pass"  >
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Remember Me
+                            </label>
+
+
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-flat m-b-30 m-t-30">Sign in</button>
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="assets/js/vendor/jquery-2.1.4.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+ 
