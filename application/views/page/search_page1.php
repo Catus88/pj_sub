85279cc2f369f870de0739230ecf6926f18d3304
jeerawat-hash <s6202041520016@email.kmutnb.ac.th>
2019-12-20 00:20:37 +0700

    ddd

--- application/views/page/search_page1.php
+++ application/views/page/search_page1.php
@@ -1,4 +1,101 @@
+  
     
+    <!-- Start Welcome area -->
+    <div class="all-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="logo-pro">
+                        <a href="index_mail.php"><img class="main-logo" src="http://mail.smcthai.co.th:8082/pj_mail/pj_sub/img/logo/logo.png" alt=""  width="120" height="20" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-advance-area">
+            <div class="header-top-area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="header-top-wraper">
+                                <div class="row">
+                                    <div class="col-lg-1 col-md-0 col-sm-1 col-xs-12">
+                                        <div class="menu-switcher-pro">
+                                            <button type="button" id="sidebarCollapse" class="btn bar-button-pro header-drl-controller-btn btn-info navbar-btn">
+                          <i class="educate-icon educate-nav"></i>
+                        </button>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-7 col-sm-6 col-xs-12">
+                                        <div class="header-top-menu tabl-d-n">
+                                            <ul class="nav navbar-nav mai-top-nav">
+                                                <li class="nav-item"><a href="index_mail.php" class="nav-link">Home</a>
+                                                </li>
+                                                  
+                                                 
+                                                 
+                                                
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                                        <div class="header-right-info">
+                                            <ul class="nav navbar-nav mai-top-nav header-right-menu">
+                                                
+                                                
+                                                <li class="nav-item">
+                                                    <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle">
+                              <img src="img/product/pro4.jpg" alt="" />
+                              <span class="admin-name">Welcome :  </span>
+                              <i class="fa fa-angle-down edu-icon edu-down-arrow"></i>
+                            </a>
+                                                    <ul role="menu" class="dropdown-header-top author-log dropdown-menu animated zoomIn">
+                                                        
+                                                        <li><a href="logout.php"><span class="edu-icon edu-locked author-log-ic"></span>Log Out</a>
+                                                        </li>
+                                                    </ul>
+
+                                                      <li class="nav-item dropdown">
+                                                    <a href="./logout.php" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle">  <i class="educate-icon educate-pages icon-wrap" aria-hidden="true"></i>
+                                                       <a href="logout.php"  ><span class="admin-name">Logout</span></a>
+                                                    
+                                                </li>
+                                                </li>
+                                                
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Mobile Menu start -->
+            <div class="mobile-menu-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="mobile-menu">
+                                <nav id="dropdown">
+                                    <ul class="mobile-menu-nav">
+                                        <li><a data-toggle="collapse" data-target="#Charts" href="index_mail.php">Home <span class="admin-project-icon edu-icon edu-down-arrow"></span></a>
+                                            <ul class="collapse dropdown-header-top">
+                                                <li><a href="index_mail.php">User Master Registration Data Entry</a></li>
+                                               
+                                            </ul>
+                                        </li>
+                                         
+                                       
+                      
+                                      
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Mobile Menu end -->
             <div class="breadcome-area">
                 <div class="container-fluid">
                     <div class="row">
@@ -16,7 +113,7 @@
                                         <ul class="breadcome-menu">
                                             <li><a href="index_mail.php">Home</a> <span class="bread-slash">/</span>
                                             </li>
-                                            <li><span class="bread-blod">More Detail  User Master Registration Data Entry</span>
+                                            <li><span class="bread-blod">Search Master Registration Data Entry</span>
                                             </li>
                                         </ul>
                                     </div>
@@ -34,7 +131,7 @@
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="product-payment-inner-st">
                             <ul id="myTabedu1" class="tab-review-design">
-                                <li class="active"><a >More Detail  User Master Registration Data Entry</a></li>
+                                <li class="active"><a >Search User Master Registration Data Entry</a></li>
  
                             </ul>
 
@@ -47,7 +144,43 @@
                                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                             <div class="review-content-section">
                                                 <div id="dropzone1" class="pro-ad">
-                                                     
+                                                    
+                                                        <div class="row  form-group">
+
+
+
+                                                             <div class="row form-group">
+                                              
+
+                            
+                           
+
+                              
+                          </div>
+                        
+                      </div>
+
+
+   
+   <div class="row form-group">
+
+                        
+
+
+     <form action="./search.php" method="post"> 
+
+                            <div class="col col-md-2"><label for="text-input" class=" form-control-label">User Name  :</label></div>
+                            <div class="col-24 col-md-8"><input type="text" id="User_name"    name="User_name" value=" " placeholder="User name"  class="form-control">
+
+                             
+                            </div>
+   <button class='btn btn-primary btn-sm'>Search</button>
+                        
+
+                          </div>
+ 
+</form>
+                             
  
                            
  <!-- Static Table Start -->
@@ -56,26 +189,11 @@
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="sparkline13-list">
-
-                            <div class="row form-group">
-                           <div class="col col-md-2"><label for="disabled-input" class=" form-control-label">A/C Code   :</label></div>
-                           <div class="  col-md-2"><input type="text" id="O1TAD1"  readonly="" name="O1TAD1" value=" " placeholder="A/C Code"  class="form-control"></div>
-
-                              <div class="col-24 col-md-8"><input type="text" id="O1TAD1"  readonly="" name="O1TAD1" value=" " placeholder="End Username"  class="form-control"></div>    
-
-                         </div>
-
-
-                            <div class="row form-group">
-                           <div class="col col-md-2"><label for="disabled-input" class=" form-control-label">Tax ID   :</label></div>
-                           <div class="col-24 col-md-2"><input type="text" id="O1TAD1"  readonly="" name="O1TAD1" value=" " placeholder="Tax ID"  class="form-control"></div>
-
-                              <div class="col col-md-2"><label for="disabled-input" class=" form-control-label">Tax Branch Code  :</label></div>
-                           <div class="col-24 col-md-2"><input type="text" id="O1TAD1"  readonly="" name="O1TAD1" value=" " placeholder="Tax Brand Code"  class="form-control"></div>
-
-                         </div>
-
-
+                            <div class="sparkline13-hd">
+                                <div class="main-sparkline13-hd">
+                                    <h1> User Master Search Results : </h1>
+                                </div>
+                            </div>
 
 
 
@@ -91,7 +209,7 @@
                                     </div>
                                          <div id="toolbar">
                                               
-  
+
                                              
                                               
                                                
@@ -103,17 +221,18 @@
                                                 
                                                  
                                                 <th data-field="id">No</th>
-                                                <th data-field="name"  >Delivery Code </th>
-                                                 <th data-field="complete">Address</th>
-                                                <th data-field="email"  >Select </th>
-                                              
+                                                <th data-field="name"  >User Name </th>
+                                                 <th data-field="complete">User Code</th>
+                                                <th data-field="email"  >Tax ID</th>
+                                                <th data-field="phone" >Branch </th>
+                                                 <th  data-field="select1"> Select</th>
+                                                 
                                                 
                                             </tr>
                                         </thead>
                                         <tbody>
                                         
- 
- 
+  
 
                                         </tbody>
                                     </table>
@@ -130,27 +249,4 @@
             </div>
         </div>
         <!-- Static Table End -->
-     
-                        
-                 
-    <div class="row form-group">
-                       <div class="card-footer" align="center">
-                        
-
-                     <a class="btn btn-danger btn-sm" href = http://mail.smcthai.co.th:8082/pj_mail/M_UMR/WEB/search.php>  Back
-                        </a>
-
-                      </div>  
-</div>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-          </form>
-
-        </div>
-
-  
+      
