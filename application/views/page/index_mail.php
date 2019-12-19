42cf2603c9d0ad5b7385470db7ed27b871bd3e65
jobsmcthai <edp-kittikarn.p@mail.smcthai.co.th>
2019-12-19 10:06:32 +0700

    22

---
+++ application/views/page/index_mail.php
@@ -0,0 +1,265 @@
+  
+
+<body>
+    <!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+    <!-- Start Left menu area -->
+  <div class="left-sidebar-pro">
+        <nav id="sidebar" class="">
+            <div class="sidebar-header">
+                <a href="index_mail.php"><img class="main-logo" src="http://mail.smcthai.co.th:8082/pj_mail/pj_sub/img/logo/logo.png" alt="" /></a>
+                <strong><a href="index_mail.php"><img src="http://mail.smcthai.co.th:8082/pj_mail/pj_sub/img/logo/logo.png" alt="" /></a></strong>
+            </div>
+            <div class="left-custom-menu-adp-wrap comment-scrollbar">
+                <nav class="sidebar-nav left-sidebar-menu-pro">
+                    <ul class="metismenu" id="menu1">
+                        <li>
+                            <a class="has-arrow" href="index_mail.php">
+                                   <span class="educate-icon educate-home icon-wrap"></span>
+                                   <span class="mini-click-non">Home</span>
+                                </a>
+                            <ul class="submenu-angle" aria-expanded="true">
+                                <li><a title="Dashboard v.1" href="index_mail.php"><span class="mini-sub-pro">User Master Registration Data Entry</span></a></li>
+                                
+                            </ul>
+                        </li>
+                         
+                             
+
+                       
+                         
+                       
+                        
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+    </div>
+    <!-- End Left menu area -->
+    <!-- Start Welcome area -->
+    <div class="all-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="logo-pro">
+                        <a href="index_mail.php"><img class="main-logo" src="http://mail.smcthai.co.th:8082/pj_mail/pj_sub/img/logo/logo.png" alt=""  width="120" height="20" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-advance-area">
+            <div class="header-top-area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="header-top-wraper">
+                                <div class="row">
+                                    <div class="col-lg-1 col-md-0 col-sm-1 col-xs-12">
+                                        <div class="menu-switcher-pro">
+                                            <button type="button" id="sidebarCollapse" class="btn bar-button-pro header-drl-controller-btn btn-info navbar-btn">
+													<i class="educate-icon educate-nav"></i>
+												</button>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-7 col-sm-6 col-xs-12">
+                                        <div class="header-top-menu tabl-d-n">
+                                            <ul class="nav navbar-nav mai-top-nav">
+                                                <li class="nav-item"><a href="index_mail.php" class="nav-link">Home</a>
+                                                </li>
+                                                 
+                                                 
+                                               
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                                        <div class="header-right-info">
+                                            <ul class="nav navbar-nav mai-top-nav header-right-menu">
+                                                
+                                                
+                                                <li class="nav-item">
+                                                    <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle">
+															<img src="http://mail.smcthai.co.th:8082/pj_mail/pj_sub/img/product/pro4.jpg" alt="" />
+															<span class="admin-name">Welcome : </span>
+															<i class="fa fa-angle-down edu-icon edu-down-arrow"></i>
+														</a>
+                                                    <ul role="menu" class="dropdown-header-top author-log dropdown-menu animated zoomIn">
+                                                       
+                                                        <li><a href="logout.php"><span class="edu-icon edu-locked author-log-ic"></span>Log Out</a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                
+
+                                                    <li class="nav-item dropdown">
+                                                    <a href="./logout.php" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle">  <i class="educate-icon educate-pages icon-wrap" aria-hidden="true"></i> </a>
+
+                                                         <a href="logout.php"  ><span class="admin-name">Logout</span></a>
+                                                    
+                                                </li>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Mobile Menu start -->
+            <div class="mobile-menu-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="mobile-menu">
+                                <nav id="dropdown">
+                                    <ul class="mobile-menu-nav">
+                                        <li><a data-toggle="collapse" data-target="#Charts" href="index_mail.php">Home <span class="admin-project-icon edu-icon edu-down-arrow"></span></a>
+                                            
+                                        </li>
+                                        
+
+                          
+
+                                        
+                                               
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Mobile Menu end -->
+            <div class="breadcome-area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="breadcome-list single-page-breadcome">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                        <div class="breadcome-heading">
+                                            
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                        <ul class="breadcome-menu">
+                                            <li><a href="index_mail.php">Home</a> <span class="bread-slash">/</span>
+                                            </li>
+                                            <li><span class="bread-blod">User Master Registration Data Entry</span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Static Table Start -->
+        <div class="data-table-area mg-b-15">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="sparkline13-list">
+                            <div class="sparkline13-hd">
+                                <div class="main-sparkline13-hd">
+                                    <h1>User Master Registration Data Entry</h1>
+                                </div>
+                            </div>
+
+
+
+                            <form method="post" >
+
+                            <div class="sparkline13-graph">
+                                <div class="datatable-dashv1-list custom-datatable-overright">
+
+                                          
+                                    
+                                    <div id="toolbar"> 
+                                       
+                                    </div>
+                                         <div id="toolbar">
+                                              
+
+                                             
+                                              <button class='btn btn-primary btn-sm' formaction="http://mail.smcthai.co.th:8082/pj_mail/M_UMR/web/index_add.php" >Create</button>  
+
+ 
+                                                  
+
+                                               
+
+                                                  
+
+                                           
+
+                                           <!------ example ----->
+                <button class='btn btn-warning btn-sm' formaction="./index_edit.php" >Edit</button> 
+
+                                           <!------ example ----->
+
+
+
+
+                                          <!------ example ----->
+               <button class='btn btn-danger btn-sm' formaction="./cancle.php" >Delete</button>
+
+                                           <!------ example ----->
+                                               
+                                                 <button class='btn btn-success btn-sm' formaction="  http://mail.smcthai.co.th:8082/PJ_MAIL/M_UMR/web/submit.php  " >Submit</button>
+
+                                    </div>
+                                   <table id="table" data-toggle="table" data-pagination="true" data-search="true"     data-key-events="true"   data-resizable="true" data-cookie="true"
+                                        data-cookie-id-table="saveId"  data-click-to-select="true" data-toolbar="#toolbar">
+                                        <thead>
+                                            <tr>
+                                                <th data-field="state" ></th>
+                                                 
+                                                <th data-field="id">No</th>
+                                                <th data-field="name"  >UMR No.</th>
+                                                <th data-field="email"  >Date</th>
+                                                <th data-field="phone" >SMC A/C</th>
+                                                <th data-field="complete">Distributor <br> Code</th>
+                                                <th data-field="complete1">Distributor <br> End User Code</th>
+                                                <th data-field="task"  >User Name</th>
+                                                 <th data-field="status"  >Status</th>
+                                                
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        
+ 
+
+
+
+
+
+
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+        </form>
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
